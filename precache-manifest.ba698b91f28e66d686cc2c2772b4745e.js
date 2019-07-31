fa63bdf6d6d03978a26d98a47d3c1c86306a5b25
James <james@Jamess-iMac.local>
2019-07-31 01:28:44 -0500

    Updates

---
+++ precache-manifest.ba698b91f28e66d686cc2c2772b4745e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d99394c513ce2924e9e5d53d36ad48",
+    "url": "/jamesgdiaz.github.io/rotometrics-ref/index.html"
+  },
+  {
+    "revision": "4d91a63694f9e3f64dda",
+    "url": "/jamesgdiaz.github.io/rotometrics-ref/static/css/2.4482b0e7.chunk.css"
+  },
+  {
+    "revision": "1f681451521d89ff7f50",
+    "url": "/jamesgdiaz.github.io/rotometrics-ref/static/css/main.1fa56576.chunk.css"
+  },
+  {
+    "revision": "4d91a63694f9e3f64dda",
+    "url": "/jamesgdiaz.github.io/rotometrics-ref/static/js/2.f091391d.chunk.js"
+  },
+  {
+    "revision": "1f681451521d89ff7f50",
+    "url": "/jamesgdiaz.github.io/rotometrics-ref/static/js/main.90591d89.chunk.js"
+  },
+  {
+    "revision": "49bb38582faafc702b91",
+    "url": "/jamesgdiaz.github.io/rotometrics-ref/static/js/runtime~main.3e5b146f.js"
+  }
+]);
