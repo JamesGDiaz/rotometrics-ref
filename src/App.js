550d4780dee6db548abd57b0396cce18e403a2b7
James <james@Jamess-iMac.local>
2019-07-31 01:57:49 -0500

    switch to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NewForm from "./components/NewForm/NewForm";
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <div className="App">
             <Header />
             <div className="Body">
@@ -29,7 +29,7 @@ class App extends Component {
               </div>
             </div>
           </div>
-        </BrowserRouter>
+        </HashRouter>
         <div />
       </div>
     );
